7f0adc4ac10f0c6d12061934213ccd573b02bdf5
thinklive1 <469631989@qq.com>
2023-09-17 23:17:36 +0800

    Site updated: 2023-09-17 23:17:36

---
+++ js/custuom.js
@@ -0,0 +1,15 @@
+
+// 记忆浏览位置
+$(function() {
+    if (localStorage.tempScrollTop) {
+      $(window).scrollTop(localStorage.tempScrollTop);
+    }
+  });
+  $(window).on("scroll", function() {
+    localStorage.setItem("tempScrollTop", $(window).scrollTop());
+  });
+  window.onbeforeunload = function() {
+    var tempScrollTop = $(window).scrollTop();
+    localStorage.setItem("tempScrollTop", tempScrollTop);
+  };
+  
