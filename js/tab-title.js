17d0a51a530af1a2972b4d9c8e1e100f20d60ed7
thinklive1 <469631989@qq.com>
2023-10-01 10:44:26 +0800

    Site updated: 2023-10-01 10:44:26

--- js/tab-title.js
+++ js/tab-title.js
@@ -1,48 +1,14 @@
-const originalTitle = document.title;
-
-let hidden = '繧「繝ェ繧ケ繧呈尔縺励※缧ゅい繝ェ繧ケ繧呈?縺励ェ縺?';
-let visible = '被发现了被发现了被发现了被发现了';
-
-let isHidden = false;
-let switchTimer = null;
-
-function updateTitle() {
-  if(isHidden) {
-    document.title = hidden;
-  } else if (switchTimer) {
-    document.title = visible;
-  } else {
-    document.title = originalTitle;
-  }
-}
-
-document.addEventListener('visibilitychange', function() {
-
-  if (document.hidden) {
-    
-    if (switchTimer) {
-      clearTimeout(switchTimer);
-      switchTimer = null;
-    }
-    
-    updateTitle();
-    switchTimer = setTimeout(() => {
-      // 隐藏后切换标题
-      isHidden = true;
-      updateTitle();
-    }, 1000);
-
-  } else {
-
-    
-    isHidden = false;
-    updateTitle();
-
-    switchTimer = setTimeout(() => {
-      switchTimer = null;
-      updateTitle();
-    }, 1000);
-
-  }
-
-});
+var OriginTitle = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         document.title = '繧「繝ェ繧ケ繧呈尔縺励※缧ゅい繝ェ繧ケ繧呈?';
+         clearTimeout(titleTime);
+     }
+     else {
+         document.title = '被发现了被发现了被发现了被发现了' + OriginTitle;
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+         }, 2000);
+     }
+ });
