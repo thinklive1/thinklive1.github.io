1dc0c8f1ac2202621d36278f216cf79e93e1b2fa
thinklive1 <469631989@qq.com>
2024-04-02 23:15:44 +0800

    Site updated: 2024-04-02 23:15:43

---
+++ js/tab-title.js
@@ -0,0 +1,14 @@
+let OriginTitle = document.title;
+let titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         document.title = '繧「繝ェ繧ケ繧呈尔縺励※缧ゅい繝ェ繧ケ繧呈?';
+         clearTimeout(titleTime);
+     }
+     else {
+         document.title = '被发现了被发现了被发现了被发现了' + OriginTitle;
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+         }, 2000);
+     }
+ });
