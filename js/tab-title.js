7095ed07c19e303179984754f727a2d8f84eda1e
thinklive1 <469631989@qq.com>
2025-09-28 14:26:13 +0800

    Site updated: 2025-09-28 14:26:11

--- js/tab-title.js
+++ js/tab-title.js
@@ -1,14 +1,16 @@
 let OriginTitle = document.title;
 let titleTime;
- document.addEventListener('visibilitychange', function () {
-     if (document.hidden) {
-         document.title = '繧「繝ェ繧ケ繧呈尔縺励※缧ゅい繝ェ繧ケ繧呈?';
-         clearTimeout(titleTime);
-     }
-     else {
-         document.title = '被发现了被发现了被发现了被发现了' + OriginTitle;
-         titleTime = setTimeout(function () {
-             document.title = OriginTitle;
-         }, 2000);
-     }
- });
+
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        titleTime = setTimeout(() => {
+            document.title = '繧「繝ェ繧ケ繧呈尔縺励※缧ゅい繝ェ繧ケ繧呈?';
+        }, 1000);
+    }
+    else {
+        document.title = '被发现了被发现了被发现了被发现了';
+        titleTime = setTimeout(() => {
+            document.title = OriginTitle;
+        }, 800);
+    }
+});
